5f1e84c5834ff09399c966876c15aacf0505876f
HyDrAa06 <stasikbg06@gmail.com>
2025-12-24 16:41:56 +0200

    Add gitignore and remove Visual Studio files

--- StudentQuest/StudentQuest.cpp
+++ StudentQuest/StudentQuest.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
+void setStatsByDifficulty(const char* difficulty, int& knowledge, 
+    int& psyche, int& energy, int& luck, int& money)
+{
+    if (!difficulty)
+    {
+        return;
+    }
+
+    if (difficulty == "easy")
+    {
+        knowledge = 80;
+        energy = money = psyche = 100;
+    }
+    else if (difficulty == "normal")
+    {
+        knowledge = 50;
+        energy = money = psyche = 80;
+    }
+    else if (difficulty == "hard")
+    {
+        knowledge = 35;
+        energy = money = 80;
+        psyche = 40;
+    }
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
